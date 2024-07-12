4338315ee95aa1637cf5a32f9f03d3bd245e0985
JohnSmith-Defender <johnsmith.diamon@gmail.com>
2024-07-12 12:28:54 -0400

    remove warnings

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -2,12 +2,6 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
-import {
-	AiFillGithub,
-	AiOutlineTwitter,
-	AiFillInstagram,
-} from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
 
 function Home2() {
 	return (
